fb7087446307eff2b4ad2099e6c1e28158eb89be
Femi Ajao <30802086+phemorah@users.noreply.github.com>
2021-09-05 22:27:05 +0100

    Server.js updated

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const { Pool } = require('pg');
 const express = require('express');
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.static(__dirname + "/dist/"));
